b356bda78dcbc9f5ed045552fc7bf36088d1a291
pixel <pixel@main.main>
2024-01-03 15:19:46 +0200

    4-print_hexa.py

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-for i in range(99):
-    print("{} = {}".format(i, hex(i)))
+for number in range(100):
+    if number == 99:
+        print("{}".format(number))
+    else:
+        print("{:02}".format(number), end=", ")
