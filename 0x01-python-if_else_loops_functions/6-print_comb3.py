583c58e6f1d0bffc58ee13d060c25a56c0c7ef51
pixel <pixel@main.main>
2024-01-03 15:48:55 +0200

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for i in range(10):
     for j in range(i + 1, 10):
-        if i != j:
-            print(f"{i}{j}", end=", " if i < 8 or j < 9 else "\n")
+        if i != j and i * 10 + j < 89:
+            print(f"{i:02d}, {j:02d}", end=", " if i < 8 or j < 9 else "\n")
