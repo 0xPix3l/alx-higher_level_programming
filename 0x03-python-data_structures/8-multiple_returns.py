15a891a12160baf3ceaf66ff1d609b4bf286dac4
pixel <piixel.1878@gmail.com>
2024-01-07 19:05:34 +0200

    all

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -6,7 +6,3 @@ def multiple_returns(sentence):
     else:
         my_tuble = (len(sentence), sentence[0])
     return my_tuble
-        
-sentence = "At school, I learnt C!"
-length, first = multiple_returns(sentence)
-print("Length: {:d} - First character: {}".format(length, first))
