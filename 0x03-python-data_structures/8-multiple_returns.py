1c1dbde3a40fed747e3e99cd196ef142608801fd
pixel <piixel.1878@gmail.com>
2024-01-07 19:04:47 +0200

    8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+def multiple_returns(sentence):
+    my_tuble = ()
+    if len(sentence) == 0:
+        my_tuble = (0, None)
+    else:
+        my_tuble = (len(sentence), sentence[0])
+    return my_tuble
+        
+sentence = "At school, I learnt C!"
+length, first = multiple_returns(sentence)
+print("Length: {:d} - First character: {}".format(length, first))
