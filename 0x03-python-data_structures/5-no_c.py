b0dfae5cecbd19e89d343fdfb7f406b9a40c8705
pixel <piixel.1878@gmail.com>
2024-01-07 16:37:48 +0200

    5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    def no_c(my_string):
-        new = ""
-        for i in my_string:
-            if i.lower() != 'c':
-                new += i
-        return new
+def no_c(my_string):
+    new = ""
+    for i in my_string:
+        if i.lower() != 'c':
+            new += i
+    return new
