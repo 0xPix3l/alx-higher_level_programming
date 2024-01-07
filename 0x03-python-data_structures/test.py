438e85b2aab18000619222d0d9d93a4be31be22d
pixel <piixel.1878@gmail.com>
2024-01-07 16:55:06 +0200

    6

--- 0x03-python-data_structures/test.py
+++ 0x03-python-data_structures/test.py
@@ -1,4 +1,7 @@
-my_list = [1,2,3,4]
+def no_c(mystring):
+    new_string = mystring.translate({ord(i): None for i in 'cC'})
+    return new_string
 
-my_list[0] = 123
-print(my_list)
+print(no_c("Best School"))
+print(no_c("Chicago"))
+print(no_c("C is fun!"))
