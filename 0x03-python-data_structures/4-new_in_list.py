6b05eb6a2705b00b139779cd7c570e01c32f080c
pixel <piixel.1878@gmail.com>
2024-01-07 16:22:50 +0200

    4-new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     def new_in_list(my_list, idx, element):
-        a = my_list.copy()
-        a[idx] = element
-        return a
+        if idx < 0 or idx > len(my_list) - 1:
+            return my_list
+        else:
+            a = my_list.copy()
+            a[idx] = element
+            return a
