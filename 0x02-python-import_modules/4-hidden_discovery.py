86c3739bcac0a5ef5576e969ab828e0335698fbe
pixel <piixel.1878@gmail.com>
2024-01-07 16:15:42 +0200

    all

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    import hidden_4
-
-    names = dir(hidden_4)
-    for name in names:
-        if name[:2] != "__":
-            print(name)
+    def new_in_list(my_list, idx, element):
+        a = my_list.copy()
+        a[idx] = element
+        return a
