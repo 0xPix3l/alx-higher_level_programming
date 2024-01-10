81c627f1b9938cebe6ecd9ee099e536d69a89dfe
pixel <piixel.1878@gmail.com>
2024-01-11 00:41:52 +0200

    all

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3]
 def number_keys(a_dictionary):
-    result = 0
-    for a in a_dictionary.items():
-        result += 1
-    return(result)
+    return(len(a_dictionary.keys()))
