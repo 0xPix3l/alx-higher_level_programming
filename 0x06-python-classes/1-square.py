486cfce689f10839aba47f43386898159c3d064e
Pixel <piixel.1878@gmail.com>
2024-01-24 03:32:06 +0200

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""Define a class Square."""
+
+
+class Square:
+    def __init__(self, size):
+        """Init a new Sqaure
+
+        Args:
+            size (int): The size of the new square
+        """
+        self.__size = size
