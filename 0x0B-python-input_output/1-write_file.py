5e2a542f9f4ad37e0f2f2a3c24d9217ba5c67116
Pixel <piixel.1878@gmail.com>
2024-02-07 00:42:06 +0200

    all

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Defines a function that write to a file."""
+
+
+def write_file(filename="", text=""):
+    with open(filename, 'w', encoding='UTF-8') as f:
+        f.write(text)
+        print(len(text))
