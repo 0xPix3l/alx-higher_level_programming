0fd7aedc349c27766ec0e291197c03be9235d031
Pixel <piixel.1878@gmail.com>
2024-02-12 03:12:35 +0200

    second_task

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 """ This module acts like a base for other classes. """
 
-
 class Base:
     """ Represent base for all classes """
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """ """
+        """ Initialize the base object and assign a unique ID if not provided. """
         if id is not None:
             self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+"""This module contains a rectangle class"""
+
+from models.base import Base
+
+
+class Rectangle(Base):
+    """A rectangle."""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Initialize the right attributes."""
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
+
+    # Getters:
+
+    @property
+    def width(self):
+        """ Get width """
+        return self.__width
+
+    @property
+    def height(self):
+        """ Get height """
+        return self.__height
+
+    @property
+    def x(self):
+        """ Get x """
+        return self.__x
+
+    @property
+    def y(self):
+        """ Get y """
+        return self.__y
+
+    # Setters:
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an int")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an int")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+
+    @x.setter
+    def x(self, value):
+        if not isinstance(value, int):
+            raise TypeError("x must be an int")
+
+    @y.setter
+    def y(self, value):
+        if not isinstance(value, int):
+            raise TypeError("y must be an int")
