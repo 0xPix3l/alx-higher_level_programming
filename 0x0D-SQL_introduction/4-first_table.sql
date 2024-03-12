10957f68a09e6afaef3cd12e842335c7a2cab9de
Pixel <piixel.1878@gmail.com>
2024-03-12 21:08:22 +0200

    all

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,5 +1,2 @@
 -- Creates a table called first_table with id and value.
-CREATE IF NOT EXISTS `first table` (
-	id INT AUTO_INCREMENT PRIMARY KEY,
-	name VARCHAR(256)
-	);
+CREATE IF NOT EXISTS `first table` (`id` INT, `name` VARCHAR(256));
