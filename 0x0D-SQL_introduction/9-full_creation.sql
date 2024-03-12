9156b79885d8a86474a76b12aa2ef1c822d7f48d
Pixel <piixel.1878@gmail.com>
2024-03-12 21:31:29 +0200

    all

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,6 @@
+-- Creates a table called `second_table`.
+CREATE TABLE IF NOT EXISTS `second_table` (`id` INT, `name` VARCHAR(256), `score` INT);
+INSERT INTO `second_table` (`id` = 1, `name` = "John", `score` = 10);
+INSERT INTO `second_table` (`id` = 2, `name` = "Alex", `score` = 3);
+INSERT INTO `second_table` (`id` = 3, `name` = "Bob", `socre` = 14);
+INSERT INTO `second_table` (`id` = 4, `name` = "George", `score` = 8);
