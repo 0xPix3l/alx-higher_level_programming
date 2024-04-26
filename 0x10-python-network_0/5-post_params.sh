f6e783d8646723b270e5518cf944647e79c2a919
Pixel <piixel.1878@gmail.com>
2024-04-26 16:40:58 +0300

    test

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # POST data
-curl -sX POST -d "email=test@gmail.com&subject=I will always be here for PLD" -L
+curl -sX POST -d $1 "email=test@gmail.com&subject=I will always be here for PLD" -L
