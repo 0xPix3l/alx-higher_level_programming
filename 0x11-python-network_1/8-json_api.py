ee19b6a0e5844f7769c5d119b3f5a72146cdae59
Pixel <piixel.1878@gmail.com>
2024-04-27 08:25:41 +0300

    test2

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+sends a post
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    letter = "" if len(sys.argv) < 2 else letter = argv[1]
+    data = {"q": letter}
+    r = requests.post("http://0.0.0.0:5000/search_user", data=data)
+    try:
+        response = r.json()
+        if response == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(response.get("id"), response.get("name")))
+    except ValueError:
+        print("Not a valid JSON")
