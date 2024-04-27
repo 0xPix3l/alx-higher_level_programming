bf9d9b46b0aee778322e285c2caf388e5d9cf59e
Pixel <piixel.1878@gmail.com>
2024-04-27 07:47:31 +0300

    test2

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -3,7 +3,7 @@
 import requests
 
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     """ fetches a website """
     r = requests.get('https://alx-intranet.hbtn.io/status')
     print('Body response:')
